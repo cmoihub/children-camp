7c5844b0cc821fdcb5d9d998807b64ebb44b7183
Craig Isesele <10642539+cmoihub@users.noreply.github.com>
2020-07-20 12:04:48 -0400

    add conditionals

---
+++ math/conditionals.py
@@ -0,0 +1,22 @@
+condition = True
+if condition:
+    print("condition met")
+# convert the following to an else statement
+# build an else if chain if you wanna challenge
+if not condition:
+    print("condition not met")
+
+meals = 3
+conditon1 = a == 3
+conditon2 = a > 5
+if condition1:
+    print("I've got enough meals")
+else if conditon2:
+    print("I've got too much to eat")
+else:
+    print("I'm small and I love it")
+
+man1 = {"age": 40}
+woman1 = {"age": 40}
+if man1["age"] >= woman1["age"]:
+    print("too old to marry the woman")
